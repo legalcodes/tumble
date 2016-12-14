b7e8a728b1f0edc9f4983cc83da08c1cd25a6c78
legalcodes <jwtippens@gmail.com>
2016-12-13 21:51:22 -0800

    Fixed formatting and added form

--- src/components/searchBlogs.jsx
+++ src/components/searchBlogs.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, Row, Col, ControlLabel, FormControl, FormGroup, Button } from "react-bootstrap";
 
 const SearchBar = () => {
+  function FieldGroup({ id, label, help, ...props }) {
+    return (
+      <FormGroup controlId={id}>
+          <FormControl {...props} />
+      </FormGroup>
+    );
+  }
+
   return (
-    <Grid>
-      <Row className="show-grid">
-        <Col sm={6} md={6} >
-          Blog name:
-        </Col>
-        <Col sm={6} md={6} >
-          Tag:
-        </Col>
-      </Row>
-      <Row className="show-grid">
-        <Col sm={6} md={6} >
-          <input/>
-        </Col>
-        <Col sm={6} md={6} >
-          <input/>
-        </Col>
-      </Row>
-    </Grid>
+      <Grid>
+          <form>
+            <Row className="show-grid">
+                <Col sm={6} md={3}>
+                    <ControlLabel>Blog name:</ControlLabel>
+                </Col>
+                <Col sm={6} md={3}>
+                    <ControlLabel>Tag:</ControlLabel>
+                </Col>
+            </Row>
+            <Row className="show-grid">
+                <Col sm={6} md={3}>
+                    <FieldGroup
+                        id="formControlsName"
+                        type="name"
+                        label="blog name"
+                        placeholder="Enter email"
+                    />
+                </Col>
+                <Col sm={6} md={3}>
+                    <FieldGroup
+                        id="formControlsName"
+                        type="name"
+                        label="blog name"
+                        placeholder="Enter email"
+                    />
+                </Col>
+            </Row>
+            <Row className="show-grid">
+                <Col sm={6} md={3}>
+                </Col>
+                <Col sm={6} md={3}>
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </Col>
+            </Row>
+          </form>
+      </Grid>
   );
 };
 
