412bd6801bb633e08cf5b3092627033694db7134
legalcodes <jwtippens@gmail.com>
2016-12-14 00:20:22 -0800

    Updated app w/ axios but failed at CORS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import axios from "axios";
 import './App.css';
 import SearchBar from './components/searchBlogs';
 
 const API_KEY = 'PBqHiwj1I0gtTrSEvL6BYjX8YbSMzFtAraiHoY7attGmdBR6AR';
+
+//TODO : setup server, configure it to accept CORS requests
+
+
+const post = "/posts";
+const user = "/peacecorps"
+const suffix = ".tumblr.com"
+const apiPrefix = "?api_key="
+//const tag = "";
+//const transformURI = () => {};
+
+const uri = `/v2/blog${user}${suffix}${post}${apiPrefix}${API_KEY}`;
+
+axios.get(uri)
+  .then((res)=>console.log("RESPONSE: ", res))
+  .catch((err)=>console.log("ERROR: ", err));
+
 
 class App extends Component {
   render() {
